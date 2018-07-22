9d18a840605d609e0d66bd538aa86c88d9535b77
eliasadam <elias-adam@hotmail.co.uk>
2018-07-22 17:58:31 +0100

    created one file and deleted the html files

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const exphbs = require("express-handlebars");
 const express = require("express");
 const app = express();
 
@@ -5,6 +6,31 @@ const app = express();
 // i.e /my-cv will server /my-cv.html
 app.use(express.static("public", {'extensions': ['html']}));
 
+app.get("/contact", function(req, res){
+res.render("contact");
+}
+);
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.get("/", (req, res) => {
+  res.render("index", {
+  title: "Elias Profile"  
+  });
+});
+
+
+
+// app.get("/contact", function(req, res){
+//   res.render("this a content page");
+
+app.get("/my-cv", (req, res) => {
+  res.render("my-cv");
+
+  app.get("/admin", (req, res) => {
+    res.render("admin");
+  });
+});
 
 // what does this line mean: process.env.PORT || 3000
 app.listen(process.env.PORT || 3000, function () {
