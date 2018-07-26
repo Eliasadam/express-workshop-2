20a1fba2b205ace5663fc18fe7af67088fe38b59
eliasadam <elias-adam@hotmail.co.uk>
2018-07-26 14:45:24 +0100

    completed most of the requested task

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const exphbs = require("express-handlebars");
 const express = require("express");
 const app = express();
-
+const posts = require("./data/posts.json")
 // The extensions 'html' allows us to serve file without adding .html at the end 
 // i.e /my-cv will server /my-cv.html
 app.use(express.static("public", {'extensions': ['html']}));
@@ -15,22 +15,34 @@ app.set("view engine", "handlebars");
 
 app.get("/", (req, res) => {
   res.render("index", {
-  title: "Elias Profile"  
+  title: "Elias Profile" 
+
   });
+});
+
+app.get("/my-posts", (req, res) => {
+  res.render("mypost", posts);
+  
 });
 
 
 
-// app.get("/contact", function(req, res){
-//   res.render("this a content page");
+
+
+
+
+ app.get("/my-post", function(req, res){
+   res.render("my-post");
+ });
 
 app.get("/my-cv", (req, res) => {
   res.render("my-cv");
+});
 
   app.get("/admin", (req, res) => {
     res.render("admin");
   });
-});
+
 
 // what does this line mean: process.env.PORT || 3000
 app.listen(process.env.PORT || 3000, function () {
